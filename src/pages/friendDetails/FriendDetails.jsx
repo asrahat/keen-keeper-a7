aa7506b013e9a18fb78bc6e3e776a2d8bb23334b
asrahat <asrahat4742@gmail.com>
2026-04-16 17:15:56 +0600

    selected friends are added

--- src/pages/friendDetails/FriendDetails.jsx
+++ src/pages/friendDetails/FriendDetails.jsx
@@ -8,14 +8,12 @@ const FriendDetails = () => {
   // console.log(params);
   const { friends, loading } = useFriends();
 
-
-
   const isFriendExist = friends.find((friend) => String(friend.id) === id);
   console.log(isFriendExist);
 
-
-   const { selectedFriends, setSelectedFriends }=useContext(SelectFriendsContext);
-
+  const { selectedFriends, setSelectedFriends } =
+    useContext(SelectFriendsContext);
+  console.log(selectedFriends);
   if (loading) {
     return (
       <div className="h-[60vh] flex justify-center items-center">
@@ -24,17 +22,35 @@ const FriendDetails = () => {
     );
   }
 
-  const handleSelectedFriends =()=>{
-    setSelectedFriends([...selectedFriends,isFriendExist]);
-    alert(`${isFriendExist.name} is Selected`)
-  }
+  const handleSelectedFriends = (type) => {
+    const alreadyExists = selectedFriends.find(
+      (friend) => friend.name === isFriendExist.name && friend.action === type,
+    );
+
+    if (alreadyExists) {
+      alert("Already Added");
+      return;
+    }
+
+    const newItem = {
+      name: isFriendExist.name,
+      action: type,
+      date: new Date().toDateString(),
+    };
+
+    setSelectedFriends([...selectedFriends, newItem]);
+    alert(`${type} scheduled with ${isFriendExist.name}`);
+  };
 
   return (
-   
     <div className="container mx-auto md:flex justify-between items-center gap-5 my-10">
       <div className="space-y-5">
         <div className="backdrop-blur-lg shadow-lg hover:shadow-2xl hover:-translate-y-2 transition-all duration-300 border flex flex-col justify-center items-center border-zinc-100 p-4 rounded-xl space-y-2">
-          <img className="w-16 h-16  rounded-full" src={isFriendExist.picture} alt="" />
+          <img
+            className="w-16 h-16  rounded-full"
+            src={isFriendExist.picture}
+            alt=""
+          />
           <h2 className="text-lg font-bold">{isFriendExist.name}</h2>
           <p
             className={`px-4 rounded-full text-white ${isFriendExist.status === "on-track" && "bg-orange-500"} ${isFriendExist.status === "almost due" && "bg-green-500"}  ${isFriendExist.status === "overdue" && "bg-red-500"} `}
@@ -55,26 +71,36 @@ const FriendDetails = () => {
           <p className="text-center text-zinc-500">{isFriendExist.email}</p>
         </div>
         <div className=" flex flex-col items-center space-y-3">
-          <button className="btn w-full backdrop-blur-lg shadow-lg hover:shadow-2xl hover:-translate-y-2 transition-all duration-300">Snooze 2 Week</button>
-          <button className="btn w-full backdrop-blur-lg shadow-lg hover:shadow-2xl hover:-translate-y-2 transition-all duration-300">Archive</button>
-          <button className="btn w-full backdrop-blur-lg shadow-lg hover:shadow-2xl hover:-translate-y-2 transition-all duration-300 text-red-500">Delete</button>
+          <button className="btn w-full backdrop-blur-lg shadow-lg hover:shadow-2xl hover:-translate-y-2 transition-all duration-300">
+            Snooze 2 Week
+          </button>
+          <button className="btn w-full backdrop-blur-lg shadow-lg hover:shadow-2xl hover:-translate-y-2 transition-all duration-300">
+            Archive
+          </button>
+          <button className="btn w-full backdrop-blur-lg shadow-lg hover:shadow-2xl hover:-translate-y-2 transition-all duration-300 text-red-500">
+            Delete
+          </button>
         </div>
       </div>
-
-
 
       <div className="space-y-5">
         <div className="grid md:grid-cols-3 gap-5">
           <div className="backdrop-blur-lg shadow-lg hover:shadow-2xl hover:-translate-y-2 transition-all duration-300 border border-zinc-100 px-12 py-6 text-center rounded-xl">
-            <h2 className="text-2xl font-bold">{isFriendExist.days_since_contact}</h2>
-            <p className="text-xl font-semibold text-zinc-500">Days Since Contact</p>
+            <h2 className="text-2xl font-bold">
+              {isFriendExist.days_since_contact}
+            </h2>
+            <p className="text-xl font-semibold text-zinc-500">
+              Days Since Contact
+            </p>
           </div>
           <div className="backdrop-blur-lg shadow-lg hover:shadow-2xl hover:-translate-y-2 transition-all duration-300 border border-zinc-100 px-12 py-6 text-center rounded-xl">
             <h2 className="text-2xl font-bold">{isFriendExist.goal}</h2>
             <p className="text-xl font-semibold text-zinc-500">Goal</p>
           </div>
           <div className="backdrop-blur-lg shadow-lg hover:shadow-2xl hover:-translate-y-2 transition-all duration-300 border border-zinc-100 px-12 py-6 text-center rounded-xl">
-            <h2 className="text-2xl font-bold">{isFriendExist.next_due_date}</h2>
+            <h2 className="text-2xl font-bold">
+              {isFriendExist.next_due_date}
+            </h2>
             <p className="text-xl font-semibold text-zinc-500">Next Due</p>
           </div>
         </div>
@@ -82,7 +108,8 @@ const FriendDetails = () => {
           <div>
             <h2 className="text-2xl font-bold">Relationship Goal</h2>
             <p className="text-xl font-semibold text-zinc-500">
-              Connect every <span className="text-black font-bold">{isFriendExist.goal}</span>
+              Connect every{" "}
+              <span className="text-black font-bold">{isFriendExist.goal}</span>
             </p>
           </div>
           <button className="btn">Edit</button>
@@ -91,22 +118,30 @@ const FriendDetails = () => {
         <div className="border border-zinc-100 shadow-xl p-4  rounded-xl">
           <h2 className="text-xl font-bold mb-3">Quick Check-In</h2>
           <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
-            <div onClick={handleSelectedFriends} className="backdrop-blur-lg shadow-lg hover:shadow-2xl hover:-translate-y-2 transition-all duration-300 border border-zinc-100  px-12 py-6 text-center rounded-xl cursor-pointer">
-            <h2 className="text-2xl font-bold">3</h2>
-            <p className="text-xl font-semibold text-zinc-500">Call</p>
-          </div>
-            <div onClick={handleSelectedFriends} className="backdrop-blur-lg shadow-lg hover:shadow-2xl hover:-translate-y-2 transition-all duration-300 border border-zinc-100  px-12 py-6 text-center rounded-xl cursor-pointer">
-            <h2 className="text-2xl font-bold">3</h2>
-            <p className="text-xl font-semibold text-zinc-500">Text</p>
-          </div>
-            <div onClick={handleSelectedFriends} className="backdrop-blur-lg shadow-lg hover:shadow-2xl hover:-translate-y-2 transition-all duration-300 border border-zinc-100  px-12 py-6 text-center rounded-xl cursor-pointer">
-            <h2 className="text-2xl font-bold">3</h2>
-            <p className="text-xl font-semibold text-zinc-500">Video</p>
-          </div>
+            <div
+              onClick={() => handleSelectedFriends("Call")}
+              className="backdrop-blur-lg shadow-lg hover:shadow-2xl hover:-translate-y-2 transition-all duration-300 border border-zinc-100  px-12 py-6 text-center rounded-xl cursor-pointer"
+            >
+              <h2 className="text-2xl font-bold">3</h2>
+              <p className="text-xl font-semibold text-zinc-500">Call</p>
+            </div>
+            <div
+              onClick={() => handleSelectedFriends("Text")}
+              className="backdrop-blur-lg shadow-lg hover:shadow-2xl hover:-translate-y-2 transition-all duration-300 border border-zinc-100  px-12 py-6 text-center rounded-xl cursor-pointer"
+            >
+              <h2 className="text-2xl font-bold">3</h2>
+              <p className="text-xl font-semibold text-zinc-500">Text</p>
+            </div>
+            <div
+              onClick={() => handleSelectedFriends("Video")}
+              className="backdrop-blur-lg shadow-lg hover:shadow-2xl hover:-translate-y-2 transition-all duration-300 border border-zinc-100  px-12 py-6 text-center rounded-xl cursor-pointer"
+            >
+              <h2 className="text-2xl font-bold">3</h2>
+              <p className="text-xl font-semibold text-zinc-500">Video</p>
+            </div>
           </div>
         </div>
       </div>
-      
     </div>
   );
 };
--- src/pages/selectFriends/SelectFriends.jsx
+++ src/pages/selectFriends/SelectFriends.jsx
@@ -1,31 +1,29 @@
 import { useContext } from "react";
 import { SelectFriendsContext } from "../../context/selectFriendsProvider";
 import callImg from '../../assets/call.png'
+import textImg from '../../assets/text.png'
+import videoImg from '../../assets/video.png'
 const SelectFriends = () => {
-  const { selectedFriends }=useContext(SelectFriendsContext);
-  console.log(selectedFriends,'selectedFriends');
+  const { selectedFriends,setSelectedFriends }=useContext(SelectFriendsContext);
+  console.log(setSelectedFriends,'selectedFriends');
   return <div className="container mx-auto my-10">
 
-    {selectedFriends.length === 0 ? (
-        <h2 className="font-bold text-4xl text-center my-5">
-          No installed apps found!
-          
-        </h2>
-      ) : (
-        selectedFriends.map((friend, ind) => {
-          return (
-            <div
-              key={ind}
-              className="backdrop-blur-lg shadow-lg hover:shadow-2xl hover:-translate-y-2 transition-all duration-300 border border-zinc-100  px-12 py-6 text-center rounded-xl cursor-pointer flex items-center gap-5 mt-4"
-            >
-                <img src={callImg} alt="" />
-                <div>
-                    <p>Text <span>with {friend.name}</span></p>
-                </div>
-            </div>
-          );
-        })
-)}
+   {selectedFriends.map((friend, idx) => (
+  <div key={idx} className=" p-3 mt-4 flex gap-3 items-center rounded-2xl backdrop-blur-lg shadow-lg hover:shadow-2xl hover:-translate-y-2 transition-all duration-300">
+    
+    {/* Icon */}
+    {friend.action === "Call" && <img src={callImg} className="w-8" />}
+    {friend.action === "Text" && <img src={textImg} className="w-8" />}
+    {friend.action === "Video" && <img src={videoImg} className="w-8" />}
+
+    {/* Text */}
+    <div>
+      <p>{friend.action} with {friend.name}</p>
+      <p className="text-sm text-gray-400">{friend.date}</p>
+    </div>
+
+  </div>
+))}
 
   </div>;
 };
